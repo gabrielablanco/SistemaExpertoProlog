74f683f4f530b7f73e99448c6891fd9326dc8f99
Gabriela <ferblanco1003@gmail.com>
2020-06-12 08:53:19 -0500

    Update enfermedades.pl

--- enfermedades.pl
+++ enfermedades.pl
@@ -1,14 +1,9 @@
-% UNIVERSIDAD DISTRITAL FRANCISCO JOSÉ DE CALDAS
-% Ejercicio #2 de corte
-%  David Aparicio
-%  Gabriela Blanco Peña
-
 tty_clear/0.
 
 :-write('----------------------------------------------------------------').
 :-nl,nl,write('Digite \' diag.\' para realizar un diagnostico').
-:-nl,nl,write('Digite \' a_sc.\' para agregar un nuevo síntoma comun de alguna enfermedad').
-:-nl,nl,write('Digite \' a_sg.\' para agregar un nuevo síntoma grave de alguna enfermedad'),nl.
+:-nl,nl,write('Digite \' a_sc.\' para agregar un nuevo sÃ­ntoma comun de alguna enfermedad').
+:-nl,nl,write('Digite \' a_sg.\' para agregar un nuevo sÃ­ntoma grave de alguna enfermedad'),nl.
 :-write('---------------------------------------------------------------').
 
 :-dynamic diagnosticoSC/2.
@@ -78,8 +73,8 @@ sintoma_grave(pielFria, dengue).
 sintoma_grave(sequedadBoca, dengue).
 sintoma_grave(inflamacionGarganta, gripe).
 sintoma_grave(esputo, gripe). %Adultos mayores
-sintoma_grave(otitisMedia, gripe). %niÃ±os
-sintoma_grave(diarrea, gripe). %niÃ±os
+sintoma_grave(otitisMedia, gripe). %niÃƒÂ±os
+sintoma_grave(diarrea, gripe). %niÃƒÂ±os
 sintoma_grave(nauseas, chikungunya).
 
 recomendacion("lavado de manos", covid19).
@@ -116,7 +111,7 @@ diagnosticoSG(X,Y,Z,A,B,W):- sintoma_grave(X,W), sintoma_grave(Y,W), sintoma_gra
 diagnosticoSG(X,Y,Z,A,B,C,W):- sintoma_grave(X,W), sintoma_grave(Y,W), sintoma_grave(Z,W), sintoma_grave(A,W), sintoma_grave(B,W), sintoma_grave(C,W).
 
 menuSC:-
-     write('-------------------------------------------------------'),nl, write('Ingrese el síntoma común que siente del siguiente listado:'),nl,write('inflamacionGarganta'),nl,write('tosSeca'),nl,write('diarrea'),nl,write('conjutivitis'),nl,write('perdidaOlfato'),nl,write('erupcionesCutaneas'),nl,write('dolorCabeza'),nl,write('fiebre'),nl,write('congestionNasal'),nl,write('dolorArticulaciones'),nl,write('dolorAbdominal'),nl,write('dolorMuscular'),nl,write('fatiga'),nl,write('-------------------------------------------------------------------------------------').
+     write('-------------------------------------------------------'),nl, write('Ingrese el sÃ­ntoma comÃºn que siente del siguiente listado:'),nl,write('inflamacionGarganta'),nl,write('tosSeca'),nl,write('diarrea'),nl,write('conjutivitis'),nl,write('perdidaOlfato'),nl,write('erupcionesCutaneas'),nl,write('dolorCabeza'),nl,write('fiebre'),nl,write('congestionNasal'),nl,write('dolorArticulaciones'),nl,write('dolorAbdominal'),nl,write('dolorMuscular'),nl,write('fatiga'),nl,write('-------------------------------------------------------------------------------------').
 
 e_sc1:-
      menuSC,nl,write('Sintoma '), read(Enfermedad),nl,diagnosticoSC(Enfermedad,Y),write(Y),nl, perfil(Y).
@@ -137,7 +132,7 @@ e_sc6:-
      menuSC,nl,write('Sintoma 1 '), read(Enfermedad1), write('Sintoma 2 '), read(Enfermedad2), write('Sintoma 3 '), read(Enfermedad3), write('Sintoma 4 '), read(Enfermedad4), write('Sintoma 5 '), read(Enfermedad5), write('Sintoma 6 '), read(Enfermedad6),diagnosticoSC(Enfermedad1,Enfermedad2,Enfermedad3,Enfermedad4,Enfermedad5,Enfermedad6,Y),write(Y),nl,perfil(Y).
 
 menuSG:-
-     write('-------------------------------------------------------------------------------------'),nl, write('Ingrese el síntoma grave que siente del siguiente listado:'),nl,write('dificultadRespirar'),nl,write('dolorAbdominal'),nl,write('sangradoEncias'),nl,write('sangradoOrina'),nl,write('pielFria'),nl,write('sequedadBoca'),nl,write('inflamacionGarganta'),nl,write('esputo'),nl,write('otitisMedia'),nl,write('diarrea'),nl,write('dolorGarganta'),nl, write('nauseas'),nl,write('-------------------------------------------------------------------------------------').
+     write('-------------------------------------------------------------------------------------'),nl, write('Ingrese el sÃ­ntoma grave que siente del siguiente listado:'),nl,write('dificultadRespirar'),nl,write('dolorAbdominal'),nl,write('sangradoEncias'),nl,write('sangradoOrina'),nl,write('pielFria'),nl,write('sequedadBoca'),nl,write('inflamacionGarganta'),nl,write('esputo'),nl,write('otitisMedia'),nl,write('diarrea'),nl,write('dolorGarganta'),nl, write('nauseas'),nl,write('-------------------------------------------------------------------------------------').
 
 e_sg1:-
      menuSG,nl,write('Sintoma '), read(Enfermedad),nl,diagnosticoSG(Enfermedad,Y),write(Y),nl,perfil(Y).
@@ -156,10 +151,10 @@ e_sg5:-
 
 
 e_sc:-
-    write('-------------------------------------------------------------------------------------'), nl, write('Digite el número de sintomas que posee de un rango de 1-5'),nl,read(Respuesta), nl, ((Respuesta==1)->e_sc1;(Respuesta==2)->e_sc2; (Respuesta==3)->e_sc3; (Respuesta==4)->e_sc4; (Respuesta==5)->e_sc5; (Respuesta==6)->e_sc6; e_sc), nl, write('----------------------------------------------------------------------------------').
+    write('-------------------------------------------------------------------------------------'), nl, write('Digite el nÃºmero de sintomas que posee de un rango de 1-5'),nl,read(Respuesta), nl, ((Respuesta==1)->e_sc1;(Respuesta==2)->e_sc2; (Respuesta==3)->e_sc3; (Respuesta==4)->e_sc4; (Respuesta==5)->e_sc5; (Respuesta==6)->e_sc6; e_sc), nl, write('----------------------------------------------------------------------------------').
 
 e_sg:-
-    write('---------------------------------------------------------------------------'), nl, write('Digite el número de sintomas que posee de un rango de 1-6'),nl,read(Respuesta), nl, ((Respuesta==1)->e_sg1;(Respuesta==2)->e_sg2; (Respuesta==3)->e_sg3; (Respuesta==4)->e_sg4; (Respuesta==5)->e_sg5; e_sg), nl, write('-------------------------------------------------------------------------').
+    write('---------------------------------------------------------------------------'), nl, write('Digite el nÃºmero de sintomas que posee de un rango de 1-6'),nl,read(Respuesta), nl, ((Respuesta==1)->e_sg1;(Respuesta==2)->e_sg2; (Respuesta==3)->e_sg3; (Respuesta==4)->e_sg4; (Respuesta==5)->e_sg5; e_sg), nl, write('-------------------------------------------------------------------------').
 
 diag:-
     write('Digite \'sc\' si la mayoria de sus sintomas son comunes/leves'), nl, write('Digite \'sg\' si la mayoria de sus sintomas son graves'),nl, read(Respuesta),nl, ((Respuesta==sc)->e_sc;e_sg).
@@ -168,32 +163,32 @@ concatenar([],L,L).
 concatenar([X|M],L,[X|Z]):-concatenar(M,L,Z).
 
 p_gripe(X):-
-     bagof(Y,recomendacion(Y,X),L),write('¿Vive en clima frío? (si/no) '),read(Rta),nl,((Rta==si)->concatenar(L,['Abriguese todo el tiempo'],A),write('¿Es usted un niño/adolescente? (si/no) '), read(Rtag),nl,((Rtag==si)->concatenar(A,['Evite usar aspirinas'],B),nl,write('RECOMENDACIONES:'),nl,write(B);write('RECOMENDACIONES'),nl,write(A));write('¿Es usted niño/adolescente? (si/no) '), read(Rta2),nl,(Rta2==si)->concatenar(L,['Evite usar aspirinas'],A),nl,write('RECOMENDACIONES:'),nl,write(A);write('RECOMENDACIOES'),nl,write(L)).
+     bagof(Y,recomendacion(Y,X),L),write('Â¿Vive en clima frÃ­o? (si/no) '),read(Rta),nl,((Rta==si)->concatenar(L,['Abriguese todo el tiempo'],A),write('Â¿Es usted un niÃ±o/adolescente? (si/no) '), read(Rtag),nl,((Rtag==si)->concatenar(A,['Evite usar aspirinas'],B),nl,write('RECOMENDACIONES:'),nl,write(B);write('RECOMENDACIONES'),nl,write(A));write('Â¿Es usted niÃ±o/adolescente? (si/no) '), read(Rta2),nl,(Rta2==si)->concatenar(L,['Evite usar aspirinas'],A),nl,write('RECOMENDACIONES:'),nl,write(A);write('RECOMENDACIOES'),nl,write(L)).
 
 p_covid19(X):-
-     bagof(Y,recomendacion(Y,X),L),write('¿Ha sufrido alguna enfermedad respiratoria? '),read(Rta),nl,((Rta==si)->concatenar(L,['Utilice siempre tapabocas'],A),write('¿Es usted mayor de 70 años?' ),read(Rta2),nl,((Rta2==si)->concatenar(A,['Matengase aislado el mayor tiempo posible'],B),nl,write('RECOMENDACIONES:'),nl,write(B);write('RECOMENDACIONES:'),nl,write(A));write('¿Es usted mayor de 70 años? '), read(Rta3),nl,((Rta3==si)->concatenar(L,['Mantengase aislado el mayor tiempo posible'],A),nl,write('RECOMENDACIONES:'),nl,write(A);write('RECOMENDACIONES:'),nl,write(L))).
+     bagof(Y,recomendacion(Y,X),L),write('Â¿Ha sufrido alguna enfermedad respiratoria? '),read(Rta),nl,((Rta==si)->concatenar(L,['Utilice siempre tapabocas'],A),write('Â¿Es usted mayor de 70 aÃ±os?' ),read(Rta2),nl,((Rta2==si)->concatenar(A,['Matengase aislado el mayor tiempo posible'],B),nl,write('RECOMENDACIONES:'),nl,write(B);write('RECOMENDACIONES:'),nl,write(A));write('Â¿Es usted mayor de 70 aÃ±os? '), read(Rta3),nl,((Rta3==si)->concatenar(L,['Mantengase aislado el mayor tiempo posible'],A),nl,write('RECOMENDACIONES:'),nl,write(A);write('RECOMENDACIONES:'),nl,write(L))).
 
 p_dengue(X):-
-     bagof(Y,recomendacion(Y,X),L),write('¿Vive en climas calidos? '),read(Rta),nl,((Rta==si)->concatenar(L,['Eliminar posibles criaderos'],A),nl,write('RECOMENDACIONES:'),nl,write(A);write('RECOMENDACIONES:'),nl,write(L)).
+     bagof(Y,recomendacion(Y,X),L),write('Â¿Vive en climas calidos? '),read(Rta),nl,((Rta==si)->concatenar(L,['Eliminar posibles criaderos'],A),nl,write('RECOMENDACIONES:'),nl,write(A);write('RECOMENDACIONES:'),nl,write(L)).
 
 p_chikungunya(X):-
-     bagof(Y,recomendacion(Y,X),L),write('¿Vive en climas calidos? '),read(Rta),nl,((Rta==si)->concatenar(L,['Utilizar ropa que lo proteja de las picaduras'],A),nl,write('RECOMENDACIONES:'),nl,write(A);write('RECOMENDACIONES:'),nl,write(L)).
+     bagof(Y,recomendacion(Y,X),L),write('Â¿Vive en climas calidos? '),read(Rta),nl,((Rta==si)->concatenar(L,['Utilizar ropa que lo proteja de las picaduras'],A),nl,write('RECOMENDACIONES:'),nl,write(A);write('RECOMENDACIONES:'),nl,write(L)).
 
 
 perfil(X):-
      ((X==gripe)->p_gripe(X);(X==covid19)->p_covid19(X);(X==dengue)->p_dengue(X);(X==chikungunya)->p_chikungunya(X);fail).
 
 a_sc:-
-     write('Digite el nombre del sintoma comun que va añadir '), read(Sintoma),nl,write('Digite el nombre de la enfermedad asociada a ese sintoma '), read(Enfermedad),nl,assert(sintoma_comun(Sintoma,Enfermedad)),repetir.
+     write('Digite el nombre del sintoma comun que va aÃ±adir '), read(Sintoma),nl,write('Digite el nombre de la enfermedad asociada a ese sintoma '), read(Enfermedad),nl,assert(sintoma_comun(Sintoma,Enfermedad)),repetir.
 
 repetir:-
-     write('¿Desea incluir otro sintoma junto con su enfermedad? (si/no) '),read(Rta),nl,((Rta==si)->a_sc;fail).
+     write('Â¿Desea incluir otro sintoma junto con su enfermedad? (si/no) '),read(Rta),nl,((Rta==si)->a_sc;fail).
 
 a_sg:-
-     write('Digite el nombre del sintoma grave que va añadir '), read(Sintoma),nl,write('Digite el nombre de la enfermedad asociada a ese sintoma '), read(Enfermedad),nl,assert(sintoma_grave(Sintoma,Enfermedad)),repetir2.
+     write('Digite el nombre del sintoma grave que va aÃ±adir '), read(Sintoma),nl,write('Digite el nombre de la enfermedad asociada a ese sintoma '), read(Enfermedad),nl,assert(sintoma_grave(Sintoma,Enfermedad)),repetir2.
 
 repetir2:-
-     write('¿Desea incluir otro sintoma junto con su enfermedad? (si/no) '),read(Rta),nl,((Rta==si)->a_sg;fail).
+     write('Â¿Desea incluir otro sintoma junto con su enfermedad? (si/no) '),read(Rta),nl,((Rta==si)->a_sg;fail).
 
 
 undo:-diagnosticoSC(_,_),fail.
